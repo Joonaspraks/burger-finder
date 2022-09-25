b03eabd59dea8c310e7bcabfe003a51d1585a6ab
Joonas Praks <joonaspraks@gmail.com>
2022-09-25 09:30:12 +0300

    geolocation

--- frontend/burger-finder/src/components/GoogleMaps.tsx
+++ frontend/burger-finder/src/components/GoogleMaps.tsx
@@ -1,12 +1,6 @@
 import { Loader } from "@googlemaps/js-api-loader";
 
 const GoogleMaps = () => {
-	function initMap(): void {
-		new google.maps.Map(document.getElementById("map") as HTMLElement, {
-			center: { lat: -34.397, lng: 150.644 },
-			zoom: 8,
-		});
-	}
 	const loader = new Loader({
 		apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
 		version: "weekly",
@@ -15,6 +9,35 @@ const GoogleMaps = () => {
 	loader.load().then(() => {
 		initMap();
 	});
+
+	function initMap(): void {
+		// default location is Tartu
+		let coordinates: google.maps.LatLngLiteral = {
+			lat: 58.378025,
+			lng: 26.728493,
+		};
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => {
+					coordinates = {
+						lat: position.coords.latitude,
+						lng: position.coords.longitude,
+					};
+					createInstance(coordinates);
+				},
+				() => {
+					createInstance(coordinates);
+				}
+			);
+		}
+	}
+
+	const createInstance = (coordinates: google.maps.LatLngLiteral) => {
+		new google.maps.Map(document.getElementById("map") as HTMLElement, {
+			center: coordinates,
+			zoom: 15,
+		});
+	};
 
 	return <div id="map" style={{ height: "500px" }} />;
 };
