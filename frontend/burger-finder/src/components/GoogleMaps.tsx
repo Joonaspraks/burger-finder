e2f8a7803b86ac523f107ddbf2ee26302c643a69
Joonas Praks <joonaspraks@gmail.com>
2022-09-26 01:28:05 +0300

    removing location detection: not part of requirements

--- frontend/burger-finder/src/components/GoogleMaps.tsx
+++ frontend/burger-finder/src/components/GoogleMaps.tsx
@@ -25,27 +25,9 @@ const GoogleMaps = ({ setVenue, setLoading }: Props) => {
 			initMap();
 		});
 
-		function initMap(): void {
-			let coordinates = TARTU;
-			if (navigator.geolocation) {
-				navigator.geolocation.getCurrentPosition(
-					(position) => {
-						coordinates = {
-							lat: position.coords.latitude,
-							lng: position.coords.longitude,
-						};
-						createInstance(coordinates);
-					},
-					() => {
-						createInstance(coordinates);
-					}
-				);
-			}
-		}
-
-		const createInstance = (coordinates: google.maps.LatLngLiteral) => {
+		const initMap = () => {
 			const map = new google.maps.Map(document.getElementById("map")!, {
-				center: coordinates,
+				center: TARTU,
 				zoom: 12,
 				disableDefaultUI: true,
 			});
@@ -54,7 +36,7 @@ const GoogleMaps = ({ setVenue, setLoading }: Props) => {
 				center: TARTU_BUSSIJAAM,
 				radius: exludedAreaRadius,
 			}).setMap(map);
-			getBurgerVenues(coordinates).then(({ results }) => {
+			getBurgerVenues(TARTU).then(({ results }) => {
 				setLoading(false);
 				results.forEach((result) => {
 					const { latitude, longitude } = result.geocodes.main;
